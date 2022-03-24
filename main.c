31c1b72ebd0a0d9f4e805ce9a12b45bbeefa226f
deblur99 <deblur99@kakao.com>
2022-03-24 22:18:09 +0900

    added-3-24

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include "operators.h"
+#include <stdbool.h>
 
-const int OPCODE = 0;
-const int OPERAND1 = 1;
-const int OPERAND2 = 2;
+enum _TARGET {
+    OPCODE = 0,
+    OPERAND1,
+    OPERAND2
+};
 
+enum _USING_REGISTER {
+    REG0 = 0,
+    REG1, REG2, REG3, REG4, REG5, REG6, REG7, REG8, REG9
+};
 
 static int registers[AMOUNT_REGS];
 
@@ -71,6 +78,103 @@ int main(int argc, char *argv[]) {
             if (tok != NULL)
                 strcpy(stringArr[idx++], tok);
         }
+        
+
+        // save register number to be used
+        int target1 = 0, target2 = 0;
+
+        // TO DO : parse string elements of stringArr
+        // check string denotes register or value
+        if (stringArr[OPERAND1][0] == 'R' && strlen(stringArr[OPERAND1] == 2)) {
+            if (isdigit(stringArr[OPERAND1][1])) {
+                switch (stringArr[OPERAND1][1]) {
+                case '0':
+                    target1 = REG0;
+                    break;
+                
+                case '1':
+                    target1 = REG1;
+                    break;
+                
+                case '2':
+                    target1 = REG2;
+                    break;
+                    
+                case '3':
+                    target1 = REG3;
+                    break;
+                
+                case '4':
+                    target1 = REG4;
+                    break;
+
+                case '5':
+                    target1 = REG5;
+                    break;
+
+                case '6':
+                    target1 = REG6;
+                    break;
+
+                case '7':
+                    target1 = REG7;
+                    break;
+
+                case '8':
+                    target1 = REG8;
+                    break;
+
+                case '9':
+                    target1 = REG9;
+                    break;
+            }
+        }
+
+        if (stringArr[OPERAND2][0] == 'R' && strlen(stringArr[OPERAND2] == 2)) {
+            if (isdigit(stringArr[OPERAND2][1])) {
+                switch (stringArr[OPERAND2][1]) {
+                case '0':
+                    target2 = REG0;
+                    break;
+                
+                case '1':
+                    target2 = REG1;
+                    break;
+                
+                case '2':
+                    target2 = REG2;
+                    break;
+                    
+                case '3':
+                    target2 = REG3;
+                    break;
+                
+                case '4':
+                    target2 = REG4;
+                    break;
+
+                case '5':
+                    target2 = REG5;
+                    break;
+
+                case '6':
+                    target2 = REG6;
+                    break;
+
+                case '7':
+                    target2 = REG7;
+                    break;
+
+                case '8':
+                    target2 = REG8;
+                    break;
+
+                case '9':
+                    target2 = REG9;
+                    break;
+                }
+            }
+        }        
 
         // TO DO : process command from "stringArr"
         int operand1 = 0, operand2 = 0;
@@ -81,6 +185,14 @@ int main(int argc, char *argv[]) {
         switch (stringArr[OPCODE][0]) {
         case 'M':
             mov(operand1, operand2);     
+        case '+':
+            add(operand1, operand2);
+        case '-':
+            sub(operand1, operand2);
+        case '*':
+            multiply(operand1, operand2);
+        case '/':
+            divide(operand1, operand2);
         }
     }
 
