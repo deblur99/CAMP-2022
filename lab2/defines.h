e95b5c3a07405e7bcd349ca9663e29337bd454e8
deblur99 <deblur99@kakao.com>
2022-04-16 02:52:10 +0900

    started-lab2

---
+++ lab2/defines.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef DEFINE_H
+#define DEFINE_H
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER      1024
+
+typedef struct _INSTRUCTOR {
+    
+}INST;
